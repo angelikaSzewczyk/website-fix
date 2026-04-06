5fd12b4e2e9eaa9ee63bdad778ead07dca359f62
Angelika Szewczyk <150901533+angelikaSzewczyk@users.noreply.github.com>
2026-04-06 12:22:54 +0200

    fix: redirect old blog slug website-langsamer-ladezeit-fixes to website-zu-langsam

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/blog/website-langsamer-ladezeit-fixes',
+        destination: '/blog/website-zu-langsam',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 module.exports = nextConfig;
